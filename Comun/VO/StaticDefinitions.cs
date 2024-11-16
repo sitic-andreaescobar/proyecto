ddfd79ccc10aee6897fada99cf412f5292fa4ceb
Andrea Escobar <andrea.escobar@siticsoftware.com>
2024-11-16 10:38:10 -0600

    Se agrega clase contenedora de enumerados y variables estáticas

---
+++ Comun/VO/StaticDefinitions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VO
+{
+    public enum eStockStatus
+    {
+        IN_STOCK = 1,
+        OUT_OF_STOCK,
+        LOW_STOCK
+    }
+
+    public enum eDbAction
+    {
+        Insert = 1,
+        Update = 2,
+        Delete
+    }
+}
