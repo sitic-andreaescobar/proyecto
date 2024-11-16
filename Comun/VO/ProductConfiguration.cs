4eee4ac809bdff285e670d7abf177268d1b8babf
Andrea Escobar <andrea.escobar@siticsoftware.com>
2024-11-16 10:37:56 -0600

    Se agrega modelo ProductConfiguration

---
+++ Comun/VO/ProductConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace VO
+{
+    [DataContract]
+    public class ProductConfiguration
+    {
+        [DataMember(EmitDefaultValue = false)]
+        public int Id { get; set; }
+        [DataMember(EmitDefaultValue = false)]
+        public int ProductId { get; set; }
+        [DataMember(EmitDefaultValue = false)]
+        public string ConfigurationName { get; set; }
+        [DataMember(EmitDefaultValue = false)]
+        public string Value { get; set; }
+        [DataMember(EmitDefaultValue = false)]
+        public DateTime CreatedAt { get; set; }
+        [DataMember(EmitDefaultValue = false)]
+        public DateTime UpdatedAt { get; set; }
+    }
+}
