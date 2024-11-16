bea46e04b6f985623477dfcae506db60dac6be33
Andrea Escobar <andrea.escobar@siticsoftware.com>
2024-11-16 10:37:34 -0600

    Se agrega modelo Order

---
+++ Comun/VO/Order.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VO
+{
+    [DataContract]
+    public class Order
+    {
+        [DataMember(EmitDefaultValue = false)]
+        public int Id { get; set; }
+        [DataMember(EmitDefaultValue = false)]
+        public double TotalPrice { get; set; }
+        [DataMember(EmitDefaultValue = false)]
+        public string Status { get; set; }
+        [DataMember(EmitDefaultValue = false)]
+        public DateTime CreatedAt { get; set; }
+        [DataMember(EmitDefaultValue = false)]
+        public DateTime UpdatedAt { get; set; }
+    }
+}
