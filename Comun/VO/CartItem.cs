0e7ed02dfb3a593b62c90d85eb6297224d10ab79
Andrea Escobar <andrea.escobar@siticsoftware.com>
2024-11-16 10:37:11 -0600

    Se agrega modelo CartItem

---
+++ Comun/VO/CartItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VO
+{
+    public class CartItem
+    {
+        public int Id { get; set; }
+        public int CartId { get; set; }
+        public int ProductId { get; set; }
+        public int Quantity { get; set; }
+        public double Price { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
