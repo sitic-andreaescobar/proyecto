87b63b717ac6d9e04bc8c03c26c879a839323f44
Andrea Escobar <andrea.escobar@siticsoftware.com>
2024-11-16 10:37:05 -0600

    Se agrega modelo Cart

---
+++ Comun/VO/Cart.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VO
+{
+    [DataContract]
+    public class Cart
+    {
+        [DataMember(EmitDefaultValue = false)]
+        public int Id { get; set; }
+        [DataMember(EmitDefaultValue = false)]
+        public DateTime CreatedAt { get; set; }
+        [DataMember(EmitDefaultValue = false)]
+        public DateTime UpdatedAt { get; set; }
+    }
+}
