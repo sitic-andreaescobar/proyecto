1f3fe2a6dbebbbef128a9c283fe23bce026e0d7f
Andrea Escobar <andrea.escobar@siticsoftware.com>
2024-11-22 21:00:53 -0600

    Se configuran rutas en Proyecto y se agregan clases DAL y BLL

---
+++ BLL/ProductBLL/ProductDAL.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    internal class ProductDAL
+    {
+        #region Variables & Properties
+
+        #endregion
+
+        #region Constructors
+
+        #endregion
+
+        #region Methods
+
+        #endregion
+    }
+}
