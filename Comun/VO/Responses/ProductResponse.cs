857b65288dd9edb62cdd71e673b0da4cc3536c9e
Andrea Escobar <andrea.escobar@siticsoftware.com>
2024-11-23 12:55:32 -0600

    Se agregan responses

---
+++ Comun/VO/Responses/ProductResponse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VO
+{
+    [DataContract]
+    public class ProductResponse : Response
+    {
+        [DataMember (EmitDefaultValue = false)]
+        public Product Product { get; set; }
+        [DataMember(EmitDefaultValue = false)]
+        public List<Product> Products { get; set; }
+    }
+}
---
+++ Comun/VO/Responses/Response.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VO
+{
+    [DataContract]
+    public class Response
+    {
+        [DataMember (EmitDefaultValue = false)]
+        public Error Error { get; set; }
+        [DataMember(EmitDefaultValue = false)]
+        public bool IsSucess { get; set; }
+    }
+}
