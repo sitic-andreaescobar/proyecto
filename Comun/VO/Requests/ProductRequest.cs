eb7f8ac0686b7fc8bfdc0ee68317715ff99e1de5
Andrea Escobar <andrea.escobar@siticsoftware.com>
2024-11-23 12:55:17 -0600

    Se agregan objetos requests

---
+++ Comun/VO/Requests/ProductRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VO
+{
+    [DataContract]
+    public class ProductRequest : Request
+    {
+        [DataMember(EmitDefaultValue = false)]
+        public Product Product { get; set; }
+    }
+}
