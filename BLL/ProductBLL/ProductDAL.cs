744968f7af6d0be356909b14a95c5a76fec99122
Andrea Escobar <andrea.escobar@siticsoftware.com>
2024-11-23 11:41:50 -0600

    Se sube BLL y DAL de Product

--- BLL/ProductBLL/ProductDAL.cs
+++ BLL/ProductBLL/ProductDAL.cs
@@ -1,23 +1,69 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using VO;
 
 namespace DAL
 {
     internal class ProductDAL
     {
         #region Variables & Properties
+        private readonly DAO.DAOClass _dao = null;
 
+        internal DAO.DAOClass Dao { get { return _dao; } }
         #endregion
 
         #region Constructors
-
+        internal ProductDAL(DAO.DAOClass dao)
+        {
+            _dao = dao;
+        }
         #endregion
 
         #region Methods
+        internal DataTable GetById(int id)
+        {
+            using(SqlCommand sqlCommand = new())
+            {
+                SqlParameterCollection parameters = sqlCommand.Parameters;
+                parameters.Add("@Id", SqlDbType.Int).Value = id;
+                return _dao.QueryInformation($"{Schema.Products}.{Procedures.GetById}", parameters);
+            }
+        }
 
-        #endregion
+        internal DataTable GetAll()
+        {
+            return _dao.QueryInformation($"{Schema.Products}.{Procedures.GetAll}");
+        }
+
+        internal bool Insert(Product product)
+        {
+            SqlParameterCollection parameters = Utilities.CommonUtils.AddParametersFromObject<Product>(product);
+            parameters["@Id"].Direction = ParameterDirection.Output;
+
+            return (_dao.ExecuteProcedureWithIdentity($"{Schema.Products}.{Procedures.Insert}", parameters) > 0) ? 
+                Dao.Identity > 0 : false;            
+        }
+
+        internal bool Update(Product product)
+        {        
+            SqlParameterCollection parameters = Utilities.CommonUtils.AddParametersFromObject<Product>(product);           
+            return _dao.ExecuteProcedure($"{Schema.Products}.{Procedures.Update}", parameters) > 0;            
+        }
+
+        internal bool Delete(int id)
+        {
+            using (SqlCommand sqlCommand = new())
+            {
+                SqlParameterCollection parameters = sqlCommand.Parameters;
+                parameters.Add("@Id", SqlDbType.Int).Value = id;
+                return _dao.ExecuteProcedure($"{Schema.Products}.{Procedures.Delete}", parameters) > 0;
+            }
+        }
+        #endregion        
     }
 }
