c0f407d7a32b4e7832ba1701351e9f1d90c5e6f8
Andrea Escobar <andrea.escobar@siticsoftware.com>
2024-11-22 21:02:52 -0600

    Se agregan primeras clases estaticas de procedimientos y esquemas, asi como tipos de errores

--- Comun/VO/StaticDefinitions.cs
+++ Comun/VO/StaticDefinitions.cs
@@ -19,4 +19,85 @@ namespace VO
         Update = 2,
         Delete
     }
+
+    public enum eErrorType
+    {
+        None = 0,       //Ninguno
+        NotFound = 1,   //No se encontró registro o cualquier dato
+        Format,         //Error de conversión
+        Sql,            //Error de sql
+        Validation,     //Errores controlados
+        Connection,     //Error de conexión
+        Timeout,        //Error de timeout por bloqueos o interbloqueos
+        Unknown = 99    //Desconocido
+    }
+
+    //Representaciones
+
+    /// <summary>
+    ///  Representa los diferentes esquemas que se manejan en este microservicio.
+    /// </summary>
+    public class Schema
+    {
+        /// <summary>
+        /// Esquema de Products
+        /// </summary>
+        public static readonly string Products = "Products";
+
+        /// <summary>
+        /// Esquema de Product_Configurations
+        /// </summary>
+        public static readonly string ProductConfigurations = "Product_Configurations";
+
+        /// <summary>
+        /// Esquema de Carts
+        /// </summary>
+        public static readonly string Carts = "Carts";
+
+        /// <summary>
+        /// Esquema de Cart_Items
+        /// </summary>
+        public static readonly string CartItems = "Cart_Items";
+
+        /// <summary>
+        /// Esquema de Orders
+        /// </summary>
+        public static readonly string Orders = "Orders";
+
+        /// <summary>
+        /// Esquema de Order_Items
+        /// </summary>
+        public static readonly string OrderItems = "Order_Items";
+    }
+
+    /// <summary>
+    ///  Representa los diferentes procedimientos que se manejan en este microservicio.
+    /// </summary>
+    public class Procedures
+    {
+        /// <summary>
+        /// Procedimiento para insertar
+        /// </summary>
+        public static readonly string Insert = "Insert";
+
+        /// <summary>
+        /// Procedimiento para actualizar
+        /// </summary>
+        public static readonly string Update = "Update";
+
+        /// <summary>
+        /// Procedimiento para eliminar
+        /// </summary>
+        public static readonly string Delete = "Delete";
+
+        /// <summary>
+        /// Procedimiento para obtener un registro por identificador
+        /// </summary>
+        public static readonly string GetById = "GetById";
+
+        /// <summary>
+        /// Procedimiento para obtener todos los registros
+        /// </summary>
+        public static readonly string GetAll = "GetAll";        
+    }
 }
